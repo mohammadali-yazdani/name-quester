7aa23f5b1cd4b58c892b7acdd2bfcd853c515459
MohammadAli <aliyazdanim2@gmail.com>
2024-01-10 12:10:41 +0330

    feat: add name card component separate

---
+++ components/Card/Name.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+interface NameProps {
+  name: string;
+}
+
+const props = defineProps<NameProps>();
+</script>
+
+<template>
+  <div class="cards-box">
+    {{ name }}
+    <p>×</p>
+  </div>
+</template>
+
+<style scoped>
+.cards-box {
+  position: relative;
+  background-color: rgb(27, 61, 138);
+  color: white;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin-right: 0.5rem;
+  margin-bottom: 1rem;
+}
+
+.cards-box p {
+  position: absolute;
+  top: -30%;
+  left: 92.5%;
+  cursor: pointer;
+  color: rgba(255, 255, 255, 0.178);
+}
+</style>
